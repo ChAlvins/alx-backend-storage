e0144b7d8784d7f9d2e29eed80ea2c5bb0123514
vpnchengo <charo.alvin@gmail.com>
2023-08-09 22:14:44 +0300

    task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 --creates a table users following these requirements
 --id(integer), email(string255 chars), name(string 255 chars)
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
