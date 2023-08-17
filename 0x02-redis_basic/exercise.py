8272c7959b210d3e1ab0547d40c2fe6029c3dd05
vpnchengo <charo.alvin@gmail.com>
2023-08-17 01:26:16 +0000

    added call_history function and wrapped function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,6 +19,21 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """store the history of inputs and outputs for a particular function."""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """"wrap decorated function and return the wrapper"""
+        input_key = "{}:inputs".format(method.__qualname__)
+        output_key = "{}:ouputs".format(method.__qualname__)
+        input = str(args)
+        self._redis.rpush(input_key, input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(output_key, output)
+        return output
+    return wrapper
+
+
 class Cache:
     """a cache redis class"""
     def __init__(self):
